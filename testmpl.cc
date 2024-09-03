0e24783f536a0eab29cf614287c6256e7bd859e5
Marcel <marcel@ru.is>
2024-09-03 10:05:13 +0000

    Also output altitude.

--- testmpl.cc
+++ testmpl.cc
@@ -7,6 +7,7 @@ int main()
 	MPL3115A2 sensor;
 
 	std::cout << "Pressure " << sensor.pressure() << " hPa" << std::endl
+		  << "Altitude " << sensor.altitude() << " m"
 		  << "Temperature " << sensor.temperature() << " C"
 		  << std::endl;
 }
